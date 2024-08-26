9c676f6987871646ddaabac56c0b981117dc1599
sandermelles@gmail.com <sandermelles@gmail.com>
2024-08-26 13:23:16 +0300

    init

---
+++ SiCalc/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace SiCalc
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("Cairo-ExtraLight.ttf", "Cairo-ExtraLight");
+					fonts.AddFont("Cairo-Light.ttf", "Cairo-Light");
+				});
+
+#if DEBUG
+			builder.Logging.AddDebug();
+#endif
+
+			return builder.Build();
+		}
+	}
+}
