808783c1e48a54e7dccc436ba08494747d25f8fa
sandermelles@gmail.com <sandermelles@gmail.com>
2024-08-27 12:49:39 +0300

    ScreenSize

--- SiCalc/App.xaml.cs
+++ SiCalc/App.xaml.cs
@@ -1,10 +1,32 @@
-﻿namespace SiCalc
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+namespace SiCalc
 {
 	public partial class App : Application
 	{
-		public App()
+		const int WindowWidth = 500;
+        const int WindowHeight = 800;
+
+        public App()
 		{
 			InitializeComponent();
+
+#if WINDOWS
+		Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+		{
+			var mauiWindows = handler.VirtualView;
+			var nativeWindow = handler.PlatformView;
+			nativeWindow.Activate();
+			IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+			WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+			AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+			appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+		});
+#endif
 
 			MainPage = new AppShell();
 		}
